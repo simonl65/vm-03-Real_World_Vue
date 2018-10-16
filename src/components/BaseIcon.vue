98d3907c6456032dc6e275bacd61a69901fe7256
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-16 15:51:32 +0100

    07. Global Components

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="icon-wrapper">
+    <svg
+      :height="height" 
+      :width="width"
+      class="icon"
+    >
+      <use v-bind="{'xlink:href':'/img/feather-sprite.svg#'+name}"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
