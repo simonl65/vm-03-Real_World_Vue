d7c5a6320e7d183644c38158f20cd86b15b240bb
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-17 16:00:03 +0100

    Clean-up EventCreate rady for lesson 12

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,34 +1,11 @@
 <template>
   <div>
-    <h1>Create an event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.id }}</p>
-
-    <p>Event data: {{ getEventById(3) || 'No event data' }}</p>
-
-    <p>There are {{ categoriesLength }} categories:</p>
-    <ul>
-      <li
-        v-for="category in categories"
-        :key="category"
-      >
-        {{ category }}
-      </li>
-    </ul>
+    <h1>Create an event</h1>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-
-export default {
-  computed: {
-    categoriesLength() {
-      return this.$store.getters.catLength
-    },
-    ...mapGetters(['getEventById']),
-    ...mapState(['user', 'categories'])
-  }
-}
+export default {}
 </script>
 
 <style scoped>
