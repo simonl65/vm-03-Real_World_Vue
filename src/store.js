eff6eefce133b2f931896b5bbfe521f6373de52f
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-17 11:07:51 +0100

    Vuex: initial state settings

--- src/store.js
+++ src/store.js
@@ -1,10 +1,21 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal wlfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {}
-});
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an event, {{ userName }}</h1>
+    <p>This event was created by {{ userName }}</p>
+    <p>User ID = {{ userID }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    },
+
+    userID() {
+      return this.$store.state.user.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
