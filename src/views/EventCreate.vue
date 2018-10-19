75aa2f637a27850ab4a90ffca8596c0cc6516725
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-19 12:34:11 +0100

    Point event creation to new user module

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -91,7 +91,7 @@ export default {
     },
 
     createFreshEventObject() {
-      const user = this.$store.state.user
+      const user = this.$store.state.m_user.user
       const id = Math.floor(Math.random() * 10000000)
       return {
         id: id,
